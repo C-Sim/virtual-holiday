efdfff274b3fdb9afc253ef53234492c7646a77d
Tanveer Bassi <tanveer13a551@gmail.com>
2022-05-18 18:46:35 +0100

    console long headline and punchline

--- assets/js/joke.js
+++ assets/js/joke.js
@@ -4,14 +4,13 @@ const apiKey = "ca45ec61a4msh24fe699dc35cc23p1151b5jsn5e05295b9d8f";
 
 const jokeApiButton = document.getElementById("joke-api");
 
+// target main to append joke to
+
+const mainContainer = document.getElementById("main-container-joke");
+
 const handleButtonClick = async () => {
   // requires a URL
   const url = "https://papajoke.p.rapidapi.com/api/jokes";
-
-  // requires a HTTP method
-  // optional headers
-  // optional path params
-  // optional query params
 
   const options = {
     method: "GET",
@@ -32,6 +31,10 @@ const handleButtonClick = async () => {
   console.log(randomIndex);
   const randomJoke = jokes[randomIndex];
   console.log(randomJoke);
+  const headline = randomJoke.headline;
+  console.log(headline);
+  const punchline = randomJoke.punchline;
+  console.log(punchline);
 };
 
 jokeApiButton.addEventListener("click", handleButtonClick);
@@ -40,6 +43,7 @@ console.log(jokeApiButton);
 //get path for joke
 
 //append to main
+// $ get path for joke from object
 
 //open a pull request
 
