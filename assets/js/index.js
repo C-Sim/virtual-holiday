85f825ef7392caa6cee369cfbc60c66b6a12d8d4
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-05-19 19:29:36 +0100

    test

--- assets/js/index.js
+++ assets/js/index.js
@@ -169,4 +169,30 @@ $(document).ready(() => {
   handleNavBarToggle();
 });
 
+//LOCAL STORAGE
 
+const holidaySnapsObjects = [
+  {
+    label: "Temperature",
+    key: 30,
+  },
+  {
+    label: "Song",
+    key: 22,
+  },
+];
+
+const objectsString = JSON.stringify(holidaySnapsObjects);
+
+localStorage.setItem("objectsString", objectsString);
+
+const objectsParse = JSON.parse(localStorage.getItem("objectsString"));
+
+console.log(objectsParse);
+
+// const writeToLocalStorage = (key, value) => {
+//   // stringify object value
+//   const stringifiedValue = JSON.stringify(value);
+//   // set value for each key within LS
+//   localStorage.setItem(key, stringifiedValue);
+// };
