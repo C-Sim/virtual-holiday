853343239f98c7d1e845f1afbbd2783c6cad7779
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-05-20 22:27:55 +0100

    rendering postcards

---
+++ assets/js/snaps.js
@@ -0,0 +1,12 @@
+const renderPostcards = () => {
+  //for each postcard
+
+  const renderPostcard = () => {
+    //create postcard
+    const eachPostcard = ``;
+  };
+};
+
+const onReady = () => {
+  readFromLocalStorage();
+};
