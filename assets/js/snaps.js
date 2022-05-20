4070c9f9a2fe4f100a1bbe9689c63c24075ab8c6
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-05-20 23:18:01 +0100

    temp and location render in holiday snaps

--- assets/js/snaps.js
+++ assets/js/snaps.js
@@ -1,12 +1,43 @@
-const renderPostcards = () => {
-  //for each postcard
-
-  const renderPostcard = () => {
+const renderPostcards = (postcards) => {
+  const renderPostcard = (postcard) => {
     //create postcard
-    const eachPostcard = ``;
+    const eachPostcard = ` <div class="card">
+    <div class="card-image">
+      <img
+        class="holiday-images"
+        src="./assets/images/card-image.jpg"
+        alt="Placeholder image"
+      />
+    </div>
+    <div class="card-content">
+      <p class="card-title has-text-centered">${postcard.location}</p>
+      <!-- render user holiday destination in this div  -->
+      <div class="holiday-type text-center">
+      ${postcard.temperature}
+        <!-- use template literal in js  -->
+      </div>
+    </div>
+    <div class="hastags has-text-centered">
+      <p>
+        <span class="hastag-texts">#Virtual-party-experience</span>
+        <span class="hastag-texts">#Living-my-best-life</span>
+      </p>
+    </div>
+    <div class="buttons">
+      <button  id="save-btn" class="button is-info is-align-items-center">Save</button>
+      <button  id="save-btn" class="button is-info is-align-items-center">Delete</button>
+    </div>
+  </div>`;
+    return eachPostcard;
   };
+  return postcards.map(renderPostcard).join("");
 };
 
 const onReady = () => {
-  readFromLocalStorage();
+  const postcards = readFromLocalStorage("postcards");
+
+  const postcardsHTML = renderPostcards(postcards);
+  document.querySelector(".main-container").innerHTML = postcardsHTML;
 };
+
+document.addEventListener("DOMContentLoaded", onReady);
