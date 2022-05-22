eb44dd11d8bbaa14b6cd5bb40a7f32050e26c743
Tanveer Bassi <tanveer13a551@gmail.com>
2022-05-22 20:52:28 +0100

    cleaned up pseudo/ comments code

--- assets/js/localStorage.js
+++ assets/js/localStorage.js
@@ -1,8 +1,6 @@
 const readFromLocalStorage = (key, defaultValue) => {
-  //get from LS with key name
   const dataFromLS = localStorage.getItem(key);
 
-  //parse data
   const parsedData = JSON.parse(dataFromLS);
 
   if (parsedData) {
@@ -13,7 +11,6 @@ const readFromLocalStorage = (key, defaultValue) => {
 };
 
 const writeToLocalStorage = (key, value) => {
-  //value turned to string
   const stringifiedValue = JSON.stringify(value);
 
   localStorage.setItem(key, stringifiedValue);
